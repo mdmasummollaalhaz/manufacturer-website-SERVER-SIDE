3fb22a698c618b48d7d3794fa977f47986a3a418
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-23 07:55:57 +0600

    Server setup and dependencies installed

---
+++ __steps.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * ---------------------
+ * Server setup
+ * -------------------------
+ * 1. npm init -y
+ * 2. npm i express cors mongodb dotenv
+ * 3. index.js
+ * 4. .env
+ * 5. .gitignore
+ * 6. package - start-dev
+ * 7. npm i jsonwebtoken
+ * 8. npm i nodemon
+ * 9. npm install --save-dev nodemon
+ * 
+ */
